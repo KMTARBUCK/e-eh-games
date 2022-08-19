e7010feee7e60724e9375ee497022e94cdd77e15
Amy-O <amyalice88@gmail.com>
2022-08-19 16:02:42 +0100

    set up GameBuilder class & methods

---
+++ LetterMatch/Models/GameBuilder.cs
@@ -0,0 +1,44 @@
+namespace LetterMatch.Models;
+
+public class GameBuilder
+{
+   int index1;
+   int index2;
+   string word;
+  public GameBuilder(string word)
+  {
+    this.index1 = 1;
+    this.index2 = 3;
+    this.word = word;
+  }
+
+  public WordCombo Setup(string word)
+  {
+    WordCombo wordPair = new WordCombo();
+    wordPair.FullWord = word;
+    wordPair.MissingLetters = GetLetters(word);
+    wordPair.IncompleteWord = Split(word);
+    wordPair.Status = "incomplete";
+    return wordPair;
+  }
+
+  public string GetLetters(string word)
+  {
+    return word[this.index1].ToString() + word[this.index2].ToString();
+  }
+  public string Split(string word)
+  {
+    System.Text.StringBuilder builder = new System.Text.StringBuilder();
+    foreach(char letter in word)
+    {
+      if(letter == word[this.index1] || letter == word[this.index2])
+      {
+        builder.Append("_");
+      }
+      else{
+        builder.Append(letter);
+      }
+    }
+    return builder.ToString();
+  }
+}
