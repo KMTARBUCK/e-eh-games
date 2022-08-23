f246dea24387b17209e0336da98f7828b3d79172
Amy-O <amyalice88@gmail.com>
2022-08-23 10:33:33 +0100

    edited Game model

--- LetterMatch/Models/Game.cs
+++ LetterMatch/Models/Game.cs
@@ -5,8 +5,5 @@ public class Game
   [Key]
   public int Id {get; set;}
   public int? Level {get; set;}
-  public int UserId {get; set;}
-  public DateTime DateTimePosted {get; set;}
-  public int Likes {get; set;}
-  public User? User {get; set;}
+  public int Words {get; set;}
 }
