687c1855a89553274b6d05b7f126464e0c27b855
Amy-O <amyalice88@gmail.com>
2022-08-24 16:21:20 +0100

    attempted mocking test that needs attention

--- LetterMatch.Tests/GameMessageTest.cs
+++ LetterMatch.Tests/GameMessageTest.cs
@@ -2,6 +2,7 @@ namespace LetterMatch.Tests;
 using LetterMatch.Models; 
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;   
+using NSubstitute;
 
 public class GameMessageTest
 {
@@ -18,30 +19,29 @@ public class GameMessageTest
       driver.Quit();
     }
 
-    [Test]
-    public void GameIndex_ClickMatchingPair_RedirectToCorrectResultPage()
-    {
-        driver.Navigate().GoToUrl("http://127.0.0.1:5112"); 
-        IWebElement nameField = driver.FindElement(By.Id("username"));
-        nameField.SendKeys("Bruce"); 
-        IWebElement submitButton = driver.FindElement(By.Id("submit"));
-        submitButton.Click();
-        // IWebElement gameGreeting = driver.FindElement(By.Id("greeting"));
-      //   string userGreeting = gameGreeting.GetAttribute("innerHTML");
-      //   Assert.That(userGreeting, Does.Contain("Bruce"));
-      // // // [Test]
-      // // public void GameIndex_ClickMatchingPair_RedirectToCorrectResultPage()
-      //   driver.Navigate().GoToUrl("http://127.0.0.1:5112/game"); 
-        IWebElement incompleteWord = driver.FindElement(By.Name("incompleteWord"));
-        IWebElement missingLetter = driver.FindElement(By.Name("missingLetters"));
-        incompleteWord.Click();
-        missingLetter.Click();
-        IWebElement playButton = driver.FindElement(By.Id("play"));
-        playButton.Click();
-        string currentUrl = driver.Url;
-        Assert.AreEqual("http://127.0.0.1:5112/game?result=correct", currentUrl);
-        IWebElement resultMessage = driver.FindElement(By.Id("message"));
-        string resultMessageContent = resultMessage.GetAttribute("innerHTML");
-        Assert.That(resultMessageContent, Does.Contain("Well done!"));
-    }
+    // [Test]
+    // public void GameIndex_ClickMatchingPair_RedirectToCorrectResultPage()
+    // {
+    //     GameBuilder fakeGameBuilder = Substitute.For<GameBuilder>();
+    //     fakeGameBuilder
+    //     fakeGameBuilder.Setup({GHOST}); //this is wrong, need to figure out syntax
+
+    //     driver.Navigate().GoToUrl("http://127.0.0.1:5112"); 
+    //     IWebElement nameField = driver.FindElement(By.Id("username"));
+    //     nameField.SendKeys("Bruce"); 
+    //     IWebElement submitButton = driver.FindElement(By.Id("submit"));
+    //     submitButton.Click();
+
+    //     IWebElement incompleteWord = driver.FindElement(By.Name("incompleteWord"));
+    //     IWebElement missingLetter = driver.FindElement(By.Name("missingLetters"));
+    //     incompleteWord.Click();
+    //     missingLetter.Click();
+    //     IWebElement playButton = driver.FindElement(By.Id("play"));
+    //     playButton.Click();
+    //     string currentUrl = driver.Url;
+    //     Assert.AreEqual("http://127.0.0.1:5112/game?result=correct", currentUrl);
+    //     IWebElement resultMessage = driver.FindElement(By.Id("message"));
+    //     string resultMessageContent = resultMessage.GetAttribute("innerHTML");
+    //     Assert.That(resultMessageContent, Does.Contain("Well done!"));
+    // }
 }
